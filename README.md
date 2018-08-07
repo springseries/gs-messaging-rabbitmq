68c168340d99834ddaa752a022b7f80d2d636b8f
maskleo <masque@aliyun.com>
2018-08-07 18:16:31 +0800

    add doc

---
+++ README.md
@@ -0,0 +1,16 @@
+#
+
+## Install
+
+- [先安装 erlang](http://www.erlang.org/downloads)
+- [再安装 MQ](http://www.rabbitmq.com/)
+
+
+## Use
+
+- [web 控制台(guest/guest)](http://localhost:15672)
+
+
+## Reference
+
+- [消息队列系列（二）：.Rabbitmq的使用及Web监控工具使用](https://www.cnblogs.com/gossip/p/4475978.html)   
--- complete/src/test/java/hello/ApplicationTest.java
+++ complete/src/test/java/hello/ApplicationTest.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *//*
+
 
 package hello;
 
@@ -46,3 +47,4 @@ public class ApplicationTest {
     }
 
 }
+*/
